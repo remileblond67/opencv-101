910e75d89fd9b8063aeae6f28ee64cf4346fcf29
Rémi LEBLOND <leblond.remi@gmail.com>
2017-12-20 15:14:35 +0100

    Utilisation d'une image de type Mat

--- src/opencv-webcam.cpp
+++ src/opencv-webcam.cpp
@@ -1,5 +1,7 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/objdetect.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
@@ -11,28 +13,28 @@ main ()
 {
   char key = ' ';
   // Flux WebCam
-  CvCapture *webcamCapture;
+  VideoCapture webcamCapture(0);
   // Image capturée par la WebCam
-  IplImage *webcamImage;
+  Mat webcamImage;
   // Nom de la fenêtre d'affichage de l'image de la WebCam
   char winWebCamName[] = "Image WebCam";
 
-  webcamCapture = cvCreateCameraCapture (CV_CAP_ANY);
-
-  if (!webcamCapture)
-    {
-      printf ("Impossible de récupérer le flux vidéo de la caméra !\n");
-      return 1;
-    }
+  // Ouverture du flux de la WebCam
+  webcamCapture.open(0);
+  if (!webcamCapture.isOpened())
+  {
+    std::cout << "Pas de flux vidéo. Merci de connecter une webcam" << std::endl ;
+    return -1;
+  }
 
   while (key != 'q' && key != 'Q')
     {
-      webcamImage = cvQueryFrame (webcamCapture);
-      cvShowImage (winWebCamName, webcamImage);
+      webcamCapture >> webcamImage;
+      webcamCapture.retrieve(webcamImage, CV_CAP_OPENNI_BGR_IMAGE);
+      resize(webcamImage, webcamImage, Size(800,600));
+      imshow(winWebCamName, webcamImage);
       key = cvWaitKey (5);
     }
 
-  cvReleaseCapture(&webcamCapture);
-  cvDestroyWindow(winWebCamName);
   return 0;
 }
